8f02b1d0e469da0b0b829e25cc0cb92fe60bab37
grandpoisson <grandpoisson@gmail.com>
2019-05-19 18:47:43 +0100

    Refactored bombBoard to  dynamic & printBoard func

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -9,5 +9,34 @@ const generatePlayerBoard = ( numberOfRows, numberOfColumns) => {
     }
     return board;
 };
-generatePlayerBoard(4,4);
-console.log(board);
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    board = [];
+    for(let i=0; i < numberOfRows; i++){
+        row = [];
+        for (let j=0; j < numberOfColumns; j++){
+            row.push(null);
+        }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced ++;
+        /*NOTE: The code in your while loop has the potential to place bombs 
+        on top of already existing bombs. This will be fixed when you learn 
+        about control flow.*/
+    }
+    return board;
+};
+
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+
+};
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Player Board: '+ printBoard(playerBoard));
+console.log('  Bomb Board: '+ printBoard(bombBoard));
