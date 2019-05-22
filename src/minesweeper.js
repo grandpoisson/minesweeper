e31a441327f2dfb206959b456755dcd0b7b54d12
grandpoisson <grandpoisson@gmail.com>
2019-05-22 17:31:25 +0100

    Break to drive home - step 12

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -23,14 +23,31 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     while (numberOfBombsPlaced < numberOfBombs) {
         let randomRowIndex = Math.floor(Math.random() * numberOfRows);
         let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-        board[randomRowIndex][randomColumnIndex] = 'B';
+        if (!board[randomRowIndex][randomColumnIndex] ==='B'){
+            board[randomRowIndex][randomColumnIndex] = 'B';
         numberOfBombsPlaced ++;
+        }
+        
         /*NOTE: The code in your while loop has the potential to place bombs 
         on top of already existing bombs. This will be fixed when you learn 
         about control flow.*/
     }
     return board;
 };
+const getNumberOfNeighborBombs = (bombBoard, rowIndex,columnIndex) =>{
+   const neighbourOffsets = [
+       [-1,-1],
+       [-1,0],
+       [-1,1],
+       [0,-1],
+       [0,1],
+       [1,-1],
+       [1,0],
+       [1,1]
+   ] ;
+   const numberOfRows = bombBoard.length;
+   const numberOfColumns = bombBoard[0].length;
+}
 
 const printBoard = board => {
     console.log(board.map(row => row.join(' | ')).join('\n'));
